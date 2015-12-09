8e23a7648f571514cd9ce219803297921add5149
Фоминых Алексей Андреевич <foma1994@yandex.ru>
2015-12-09 04:59:55 +0300

    Добавил форму для ввода логина и пароля для подключения к серверу и получению списка баз данных

---
+++ Система Тестирования/Forms/ServerMainUserAuth.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Система_Тестирования
+{
+    partial class ServerMainUserAuth
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connectToServer_Button = new System.Windows.Forms.Button();
+            this.back_Button = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.password_TextBox = new System.Windows.Forms.TextBox();
+            this.name_TextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // connectToServer_Button
+            // 
+            this.connectToServer_Button.Location = new System.Drawing.Point(119, 98);
+            this.connectToServer_Button.Name = "connectToServer_Button";
+            this.connectToServer_Button.Size = new System.Drawing.Size(145, 23);
+            this.connectToServer_Button.TabIndex = 5;
+            this.connectToServer_Button.Text = "Подключиться к серверу";
+            this.connectToServer_Button.UseVisualStyleBackColor = true;
+            // 
+            // back_Button
+            // 
+            this.back_Button.Location = new System.Drawing.Point(12, 98);
+            this.back_Button.Name = "back_Button";
+            this.back_Button.Size = new System.Drawing.Size(75, 23);
+            this.back_Button.TabIndex = 4;
+            this.back_Button.Text = "Назад";
+            this.back_Button.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.password_TextBox);
+            this.groupBox1.Controls.Add(this.name_TextBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(252, 75);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ввод данных";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Пароль";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Имя входа";
+            // 
+            // password_TextBox
+            // 
+            this.password_TextBox.Location = new System.Drawing.Point(126, 45);
+            this.password_TextBox.Name = "password_TextBox";
+            this.password_TextBox.Size = new System.Drawing.Size(120, 20);
+            this.password_TextBox.TabIndex = 1;
+            this.password_TextBox.UseSystemPasswordChar = true;
+            // 
+            // name_TextBox
+            // 
+            this.name_TextBox.Location = new System.Drawing.Point(126, 19);
+            this.name_TextBox.Name = "name_TextBox";
+            this.name_TextBox.Size = new System.Drawing.Size(120, 20);
+            this.name_TextBox.TabIndex = 0;
+            // 
+            // ServerMainUserAuth
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 130);
+            this.Controls.Add(this.connectToServer_Button);
+            this.Controls.Add(this.back_Button);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ServerMainUserAuth";
+            this.Text = "Имя входа на сервер";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connectToServer_Button;
+        private System.Windows.Forms.Button back_Button;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox password_TextBox;
+        private System.Windows.Forms.TextBox name_TextBox;
+    }
+}
---
+++ Система Тестирования/Forms/ServerMainUserAuth.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Система_Тестирования
+{
+    public partial class ServerMainUserAuth : Form
+    {
+        SqlConnectionStringBuilder connectionString;
+        SelectServer_Form selectServer_Form;
+        SqlConnection connection;
+        DataTable tblDatabases;
+
+        public ServerMainUserAuth()
+        {
+            InitializeComponent();
+        }
+
+        public ServerMainUserAuth(ref SqlConnectionStringBuilder connectionString, 
+            SelectServer_Form selectServer_Form,
+            ref DataTable tblDatabases)
+        {
+            InitializeComponent();
+            Configuration();
+            this.connectionString = connectionString;
+            this.selectServer_Form = selectServer_Form;
+            this.tblDatabases = tblDatabases;
+        }
+
+        /*-------------------- Делегаты --------------------*/
+
+        private void logIn_Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connectionString.UserID = name_TextBox.Text;
+                connectionString.Password = password_TextBox.Text;
+                connection = new SqlConnection(connectionString.ConnectionString);
+                connection.Open();
+                //tblDatabases = connection.GetSchema("Databases");
+                MessageBox.Show("Подключение к серверу установлено!");
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                connectionString.UserID = "";
+                connectionString.Password = "";
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if(connection.State != ConnectionState.Closed)
+                    connection.Close();
+            }
+        }
+
+        private void back_Button_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            selectServer_Form.Show();
+        }
+
+
+        /*-------------------- Остальные функции --------------------*/
+
+        private void Configuration()
+        {
+            // Устанавливаем кнопку приёма введённых данных с формы
+            // нажатие клавиши Enter эквивалентно нажатию этой кнопки
+            // т.е. теперь при вводе данных можно сразу нажать Enter, не фокусируясь на кнопке
+            // с помощью Tab или не выбирая её с помощью мыши
+            this.AcceptButton = connectToServer_Button;
+
+            password_TextBox.UseSystemPasswordChar = true;
+
+            connectToServer_Button.Click +=
+                new EventHandler(logIn_Button_Click);
+            back_Button.Click +=
+                new EventHandler(back_Button_Click);
+
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+    }
+}
